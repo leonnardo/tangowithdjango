61e634dd0b1ce61f506475531017764188abf97e
Leonnardo Rabello <leonnardo@gmail.com>
2016-10-25 15:33:54 -0200

    Completing Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = { 'boldmessage': "Crunchy, creamy, cookie, candy, cupcake" }
-    return render(request, 'rango/index.html', context=context_dict)
+    """
+    Consulta o banco de dados por uma lista de todas as categorias atualmente armazenadas
+    Ordena as categorias pelo numero de likes de forma decrescente.
+    Pega apenas as 5 categorias com mais likes, ou todas se existirem menos de 5 categorias.
+    Poe a lista no dicionario context_dict que sera passado para o template engine.
+    """
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages': pages_list}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context = { 'name' : 'Leonnardo'}
     return render(request,'rango/about.html', context=context)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+
+
